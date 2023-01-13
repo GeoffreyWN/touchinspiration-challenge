efee431332707a7287ab86dd116db4a242ff4307
weshsnr <ndungugeoffrey11@gmail.com>
2023-01-13 16:38:21 +0300

    updated assets

---
+++ src/assets/images/index.js
@@ -0,0 +1,10 @@
+export const icons = {
+  facebookIcon: require('./facebook.png'),
+  linkedinIcon: require('./linkedin.png'),
+  twitterIcon: require('./twitter.png'),
+  emailIcon: require('./email.png'),
+}
+
+export const images = {
+  userAvatar: require('./userprofile3.jpg'),
+}
